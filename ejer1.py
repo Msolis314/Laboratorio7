42014364cfe47c62ca343877b99fdb4931a0e401
Msolis314 <solisfanny2021@gmail.com>
2023-11-27 17:58:38 -0600

    Modificacion en el printo

--- ejer1.py
+++ ejer1.py
@@ -12,15 +12,24 @@ def arguments():
     return parser.parse_args()
 
 def main():
+    """Funcion donde se toma el id y se busca la informacion usando los comandos de la terminal
+    """
     args = arguments()
     id_p = args.id
-    print("Nombre:" ,os.system(f"ps -p {id_p} -o comm="))
-    print( "Parent process id:",os.system(f" ps -p {id_p} -o pid="))
-    print("Usuario propetario:", os.system(f" ps -p {id_p} -o user="))
-    print("Porcentaje de uso de CPU:",os.system(f" ps -p {id_p} -o %cpu="))
-    print("Consumo de memoria:", os.system(f" ps -p {id_p} -o %mem="))
-    print("Estado:",os.system(f" ps -p {id_p} -o stat="))
-    print("Path del ejecutable:", os.system(f" ps -p {id_p} -o command="))
+    print("Nombre:") 
+    os.system(f"ps -p {id_p} -o comm=")
+    print( "Parent process id:")
+    os.system(f" ps -p {id_p} -o pid=")
+    print("Usuario propetario:") 
+    os.system(f" ps -p {id_p} -o user=")
+    print("Porcentaje de uso de CPU:") 
+    os.system(f" ps -p {id_p} -o %cpu=")
+    print("Consumo de memoria:")
+    os.system(f" ps -p {id_p} -o %mem=")
+    print("Estado:") 
+    os.system(f" ps -p {id_p} -o stat=")
+    print("Path del ejecutable:")
+    os.system(f" ps -p {id_p} -o command=")
 
 if __name__ == '__main__':
     main()
